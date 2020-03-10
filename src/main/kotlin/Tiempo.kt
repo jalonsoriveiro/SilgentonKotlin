067ab76c183112d31b3627c91989f97afd9d2381
jose <jalonsoriveiro@danielcastelao.org>
2020-03-11 00:08:34 +0100

    first commit

--- src/main/kotlin/Tiempo.kt
+++ src/main/kotlin/Tiempo.kt
@@ -3,15 +3,16 @@ import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
 object Tiempo {
-
+//Realizamos la conexion con la web
     var doc: Document = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").userAgent("Mozilla").get()
+    //Obtenemos los datos de los div de la pagina web
     var elementoTemperatura: Elements = doc.getElementsByClass("today_nowcard-temp")
     var temperatura:String = elementoTemperatura.text()
     var elementoTiempo: Elements = doc.getElementsByClass("today_nowcard-phrase")
     var tiempo:String = elementoTiempo.text()
 
     fun imprimir(){
-
+    //Mostrar datos
         println("$temperatura $tiempo. Datos extraídos de ${doc.title()}")
 
 
